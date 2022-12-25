a96ca7c0ab6ee8b476d636874cbe32d974467b7f
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-12-25 17:18:57 +0530

    Done

---
+++ BinarySearch.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool search_que(vector<int>A, int target){
+    int low = 0, high = A.size()-1;
+
+    while (low<=high)
+    {
+        int mid = low + (high-low);
+
+        if(target == A[mid]){
+            return true;
+            break;
+        }
+
+        else if(target > A[mid]){
+            low = mid + 1;
+        }
+
+        else if(target < A[mid]){
+            high = mid -1;
+        }
+    }
+    return false;
+}
+
+signed main(){
+    int n, k;
+    cin>>n>>k;
+
+    vector<int>A(n);
+
+    for(int i=0; i<n; i++){
+        cin>>A[i];
+    }
+
+    for(int j=0; j<k; j++){
+        int que;
+
+        cin>>que;
+
+        if(search_que(A, que)) cout<<"Yes"<<endl;
+
+        else cout<<"No"<<endl;
+    }
+
+    return 0;
+}
--- ClosestToLeft.cpp
+++ ClosestToLeft.cpp
@@ -5,36 +5,43 @@ using namespace std;
 
 int closestToLeft(vector<int>A, int target){
     int first = 0, last = A.size()-1;
+    int ans = 0;
+    int mid = first + (last - first)/2;
+    int temp = mid;
 
-    while (first<=last){
-        int mid = first + (last - first)/2;
-        if(target>A[mid]){
-            first = mid+1;
-        }
-        else if(target<=A[mid]){
-            if(target<A[mid-1])
-            last = mid-1;
+    while (first<=last){    
+        
+        if(target>A[mid]) 
+        first = mid+1;
+
+        else if(target<A[mid]) 
+        last = mid-1;
+
+        else if (target == A[mid]){
+            ans++;
+            if(target == A[temp-1]){
+            }
+            
         }
     }
     return 0;
     
 }
 
-=
 signed main(){
     int n, k;
     cin>>n>>k;
 
-    vector<int>A(n);
+    vector<int>A;
 
-    for(int i = 0; i<n; i++) cin>>A[i];
+    for(int i=0; i<n; i++) cin>>A[i];
 
-    int a;
+    int que;
 
     while (k--)
     {
-        cin>>a;
-        cout<<closestToLeft(A, a)<<endl;
+        cin>>que;
+        cout<<closestToLeft(A, que)<<endl;
 
     }
     return 0;
