94e9d04dc9a110e86c499732e219ebd4d3470b8c
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-12-27 00:34:58 +0530

    Done

--- ClosestToLeft.cpp
+++ ClosestToLeft.cpp
@@ -1,48 +1,39 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-// #define int long long;
 
 int closestToLeft(vector<int>A, int target){
     int first = 0, last = A.size()-1;
-    int ans = 0;
-    int mid = first + (last - first)/2;
-    int temp = mid;
+    int result;
 
-    while (first<=last){    
-        
-        if(target>A[mid]) 
-        first = mid+1;
+    while(first<=last){
+        int mid = first + (last - first)/2;
 
-        else if(target<A[mid]) 
-        last = mid-1;
+        if(A[mid] > target) last = mid - 1;
 
-        else if (target == A[mid]){
-            ans++;
-            if(target == A[temp-1]){
+        else if(A[mid] <= target){
+            if(A[mid+1] > target){
+                result = mid;
+                break;
             }
-            
+            else if(A[mid+1] <= target) first = mid + 1;
         }
     }
-    return 0;
-    
+    return result+1;
 }
 
 signed main(){
     int n, k;
     cin>>n>>k;
 
-    vector<int>A;
+    vector<int>A(n);
 
     for(int i=0; i<n; i++) cin>>A[i];
 
-    int que;
-
-    while (k--)
-    {
+    for(int i=0; i<k; i++){
+        int que;
         cin>>que;
-        cout<<closestToLeft(A, que)<<endl;
 
+        cout<<closestToLeft(A, que)<<endl;
     }
     return 0;
 }
